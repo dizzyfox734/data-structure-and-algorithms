dfc326a6625b5de52d5257d51e9a1e2dd6877d60
dizzyfox734 <dizzyfox734@gmail.com>
2024-02-28 00:38:56 +0900

    DLinkedList 진행중

---
+++ data-structure/DoublyLinkedList/DLinkedList.java
@@ -0,0 +1,100 @@
+package DoublyLinkedList;
+
+import Interface.List;
+
+public class DLinkedList<E> implements List<E> {
+    private Node<E> head;
+    private Node<E> tail;
+    private int size;
+
+    public DLinkedList() {
+        this.head = null;
+        this.tail = null;
+        this.size = 0;
+    }
+
+    private Node<E> search(int index) {
+        if (index < 0 || index >= size) {
+            throw new IndexOutOfBoundsException();
+        }
+
+        if (index + 1 > size / 2) {
+            Node<E> x = tail;
+            for (int i = size - 1; i > index; i--) {
+                x = x.prev;
+            }
+            return x;
+        } else {
+            Node<E> x = head;
+            for (int i = 0; i < index; i--) {
+                x = x.next;
+            }
+            return x;
+        }
+    }
+
+    public void addFirst(E value) {
+        Node<E> newNode = new Node<E>(value);
+        newNode.next = head;
+
+        if (head != null) {
+            head.prev = newNode;
+        }
+        head = newNode;
+        size++;
+        if (head.next == null) {
+            tail = head;
+        }
+    }
+
+    @Override
+    public boolean add(E value) {
+        addLast(value);
+        return true;
+    }
+
+    public void addLast(E value) {
+        Node<E> newNode = new Node<E>(value);
+
+        if (size == 0) {
+            addFirst(value);
+            return;
+        }
+
+        tail.next = newNode;
+        newNode.prev = tail;
+        tail = newNode;
+        size++;
+    }
+
+    @Override
+    public void add(int index, E value) {
+        if (index > size || index < 0) {
+            throw new IndexOutOfBoundsException();
+        }
+
+        if (index == 0) {
+            addFirst(value);
+            return;
+        }
+        if (index == size) {
+            addLast(value);
+            return;
+        }
+
+        Node<E> prevNode = search(index - 1);
+        Node<E> nextNode = prevNode.next;
+        Node<E> newNode = new Node<E>(value);
+
+        prevNode.next = null;
+        nextNode.prev = null;
+
+        prevNode.next = newNode;
+        newNode.prev = newNode;
+
+        newNode.prev = prevNode;
+        newNode.next = nextNode;
+
+        size++;
+    }
+}
---
+++ data-structure/DoublyLinkedList/Node.java
@@ -0,0 +1,14 @@
+package DoublyLinkedList;
+
+public class Node<E> {
+
+    E data;
+    Node<E> prev;
+    Node<E> next;
+
+    Node(E data) {
+        this.data = data;
+        this.prev = null;
+        this.next = null;
+    }
+}
