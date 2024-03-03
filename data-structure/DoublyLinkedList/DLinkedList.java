1deac2fad0bf00f1109ec12b4c0581dfa07d59c2
dizzyfox734 <dizzyfox734@gmail.com>
2024-03-03 23:16:35 +0900

    DLinkedList 진행중

--- data-structure/DoublyLinkedList/DLinkedList.java
+++ data-structure/DoublyLinkedList/DLinkedList.java
@@ -1,5 +1,7 @@
 package DoublyLinkedList;
 
+import java.util.NoSuchElementException;
+
 import Interface.List;
 
 public class DLinkedList<E> implements List<E> {
@@ -96,5 +98,109 @@ public class DLinkedList<E> implements List<E> {
         newNode.next = nextNode;
 
         size++;
+    }
+
+    public E remove() {
+        Node<E> headNode = head;
+
+        if (headNode == null) {
+            throw new NoSuchElementException();
+        }
+
+        E element = headNode.data;
+        Node<E> nextNode = head.next;
+
+        head.data = null;
+        head.next = null;
+
+        if (nextNode != null) {
+            nextNode.prev = null;
+        }
+
+        head = nextNode;
+        size--;
+
+        if (size == 0) {
+            tail = null;
+        }
+
+        return element;
+    }
+
+    @Override
+    public E remove(int index) {
+        if (index >= size || index < 0) {
+            throw new IndexOutOfBoundsException();
+        }
+
+        if (index == 0) {
+            return remove();
+        }
+
+        Node<E> prevNode = search(index - 1);
+        Node<E> removeNode = prevNode.next;
+        Node<E> nextNode = removeNode.next;
+
+        E element = removeNode.data;
+
+        prevNode.next = null;
+        removeNode.data = null;
+        removeNode.prev = null;
+        removeNode.next = null;
+
+        if (nextNode != null) {
+            nextNode.prev = null;
+
+            nextNode.prev = prevNode;
+            prevNode.next = nextNode;
+        } else {
+            tail = prevNode;
+        }
+
+        size--;
+
+        return element;
+    }
+
+    @Override
+    public boolean remove(Object value) {
+        Node<E> prevNode = head;
+        Node<E> x = head;
+
+        for (; x != null; x = x.next) {
+            if (value.equals(x.data)) {
+                break;
+            }
+            prevNode = x;
+        }
+
+        if (x == null) {
+            return false;
+        }
+
+        if (x.equals(head)) {
+            remove();
+            return true;
+        } else {
+            Node<E> nextNode = x.next;
+
+            prevNode.next = null;
+            x.data = null;
+            x.prev = null;
+            x.next = null;
+
+            if (nextNode != null) {
+                nextNode.prev = null;
+
+                nextNode.prev = prevNode;
+                prevNode.next = nextNode;
+            } else {
+                tail = prevNode;
+            }
+
+            size--;
+
+            return true;
+        }
     }
 }
