429a093c3a8627209aa67b61037de841a2e1f50a
dizzyfox734 <dizzyfox734@gmail.com>
2024-03-04 22:56:46 +0900

    DoubleLinkedList

--- data-structure/DoublyLinkedList/DLinkedList.java
+++ data-structure/DoublyLinkedList/DLinkedList.java
@@ -203,4 +203,71 @@ public class DLinkedList<E> implements List<E> {
             return true;
         }
     }
+
+    @Override
+    public E get(int index) {
+        return search(index).data;
+    }
+
+    @Override
+    public void set(int index, E value) {
+        Node<E> replaceNode = search(index);
+        replaceNode.data = null;
+        replaceNode.data = value;
+    }
+
+    @Override
+    public int indexOf(Object o) {
+        int index = 0;
+
+        for(Node<E> x = head; x != null; x = x.next) {
+            if (o.equals(x.data)) {
+                return index;
+            }
+            index++;
+        }
+
+        return -1;
+    }
+
+    public int lastIndexOf(Object o) {
+        int index = size;
+
+        for(Node<E> x = tail; x != null; x = x.prev) {
+            index--;
+            if (o.equals(x.data)) {
+                return index;
+            }
+        }
+
+        return -1;
+    }
+
+    @Override
+    public boolean contains(Object item) {
+        return indexOf(item) > -1;
+    }
+
+    @Override
+    public int size() {
+        return size;
+    }
+
+    @Override
+    public boolean isEmpty() {
+        return size == 0;
+    }
+
+    @Override
+    public void clear() {
+        for (Node<E> x = head; x != null;) {
+            Node<E> nextNode = x.next;
+            x.data = null;
+            x.prev = null;
+            x.next = null;
+            x = nextNode;
+        }
+        head = tail = null;
+        size = 0;
+    }
 }
