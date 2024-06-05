9367c89c2da9e041d2ed861ed8cee86807fdc82c
dizzyfox734 <dizzyfox734@gmail.com>
2024-06-05 23:57:10 +0900

    Heap 진행중

---
+++ data-structure/Heap/Heap.java
@@ -0,0 +1,56 @@
+package Heap;
+
+import java.util.Comparator;
+
+public class Heap<E> {
+
+    private final Comparator<? super E> comparator;
+    private static final int DEFAULT_CAPACITY = 10;
+
+    private int size;
+
+    private Object[] array;
+
+    public Heap() {
+        this(null);
+    }
+
+    public Heap(Comparator<? super E> comparator) {
+        this.array = new Object[DEFAULT_CAPACITY];
+        this.size = 0;
+        this.comparator = comparator;
+    }
+
+    public Heap(int capacity) {
+        this(capacity, null);
+    }
+
+    public Heap(int capacity, Comparator<? super E> comparator) {
+        this.array = new Object[capacity];
+        this.size = 0;
+        this.comparator = comparator;
+    }
+
+    private int getParent(int index) {
+        return index / 2;
+    }
+
+    private int getLeftChild(int index) {
+        return index * 2;
+    }
+
+    private int getRightChild(int index) {
+        return index * 2 + 1;
+    }
+
+    private void resize(int newCapacity) {
+        Object[] newArray = new Object[newCapacity];
+
+        for(int i = 1; i <= size; i++) {
+            newArray[i] = array[i];
+        }
+
+        this.array = null;
+        this.array = newArray;
+    }
+}
